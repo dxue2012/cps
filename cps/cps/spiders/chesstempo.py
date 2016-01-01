6db2da786617092eaa3f423cd19e002637ad2b19
Dyland Xue <dylandxue2012@gmail.com>
2016-01-02 00:44:00 +0800

    slightly better concurrency

--- cps/cps/spiders/chesstempo.py
+++ cps/cps/spiders/chesstempo.py
@@ -3,21 +3,30 @@ from scrapy.selector import Selector
 
 from cps.items import CpsItem
 
+TACTICS_URL = 'http://chesstempo.com/chess-tactics.html'
+NUM_PUZZLES_NEEDED = 100
+
+
 class chesstempoSpider(scrapy.Spider):
     name = 'chesstempo'
-    tactics_url = 'http://chesstempo.com/chess-tactics.html'
-    start_urls = [tactics_url]
+    allowed_domains = ['chesstempo.com']
+    concurrent_max = 4
+    count = 0
+
+    def request(self):
+        return scrapy.Request(TACTICS_URL, self.parse, meta={
+            'splash': {
+                'endpoint': 'render.html',
+                'args': {
+                    'wait': 10
+                }
+            }
+        }, dont_filter=True)
 
     def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url, self.parse, meta={
-                'splash': {
-                    'endpoint': 'render.html',
-                    'args': {
-                        'wait': 5
-                    }
-                }
-            })
+        i = 0
+        for i in range(self.concurrent_max):
+            yield self.request()
 
     def parse(self, response):
         # response.body is a result of a render.html call; it
@@ -25,4 +34,9 @@ class chesstempoSpider(scrapy.Spider):
         selector = Selector(response)
         item = CpsItem()
         item['FEN'] = selector.css('.ct-board').xpath('./@alt').extract_first()
-        return item
+        if item['FEN'] is not None:
+            self.count += 1
+            yield item
+
+        if self.count < NUM_PUZZLES_NEEDED:
+            yield self.request()
